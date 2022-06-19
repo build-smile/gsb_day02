5304ad29ad40242c9bbd45aacfda7c54d8b6570d
buildsmile <you@example.com>
2022-06-19 11:52:45 +0700

    fetch feeds api convert to model

--- lib/services/FeedAPI.dart
+++ lib/services/FeedAPI.dart
@@ -1,11 +1,20 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+
+import '../models/Feed.dart';
 
 class FeedService {
   String url = 'https://jsonplaceholder.typicode.com';
 
-  getFeeds() async {
+  Future<List<Feed>> getFeeds() async {
     Uri urlAPI = Uri.parse('$url/posts');
     final response = await http.get(urlAPI);
-    print(response.body);
+    if (response.statusCode == 200) {
+      final List result = jsonDecode(response.body);
+      List<Feed> data = result.map((e) => Feed.fromJson(e)).toList();
+      return data;
+    }
+    return [];
   }
 }
